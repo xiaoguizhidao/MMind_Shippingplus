2554dc689102b0ae02de6546d84e59019b5a9bc9
Giuseppe Morelli <giuseppe.morelli@magemind.com>
2014-11-20 15:45:56 +0100

    Version 1.0.1 - add import tablerate with multiple country, region, postcode divided by pipe

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 MMind_Shippingplus
 ==================
 
-Shipping Module for Magento
+Shipping Module for Magento.
 
 # Features
 - shipping price with tablerate weight/destination
 - shipping price with tablerate price/destination
 - shipping flat price (based on cart total or weight cart total)
 - free shipping
+- add import tablerate with multiple country, region, postcode divided by pipe '|'
+
+## Tablerate
+
+The module search in order: postcode, region, country.
+So if you have particular price for postcode they must be at the beginning of the tablerate.
+See tablerate_example.csv for more informations
 
 # Contributors
 - Giuseppe Morelli
 
 # Stable version
 
+1.0.1
+- add import tablerate with multiple country, region, postcode divided by pipe '|'
+
 1.0.0
+- first release
 
 # MAGENTO Installation
 
--- app/code/community/MMind/Shippingplus/Model/Resource/Shippingplus/Import.php
+++ app/code/community/MMind/Shippingplus/Model/Resource/Shippingplus/Import.php
@@ -17,179 +17,192 @@
  */
 class MMind_Shippingplus_Model_Resource_Shippingplus_Import extends Mage_Core_Model_Resource_Db_Abstract
 {
-	/**
-	 * Number of coloumns in tablerate file
-	 *
-	 * @var int
-	 */
-	protected $tablerate_cols = 7;
+    /**
+     * Number of coloumns in tablerate file
+     *
+     * @var int
+     */
+    protected $tablerate_cols = 7;
 
-	/**
-	 * Import table rates website ID
-	 *
-	 * @var int
-	 */
-	protected $_importWebsiteId = 0;
+    /**
+     * Import table rates website ID
+     *
+     * @var int
+     */
+    protected $_importWebsiteId = 0;
 
-	/**
-	 * Errors in import process
-	 *
-	 * @var array
-	 */
-	protected $_importErrors = array();
+    /**
+     * Errors in import process
+     *
+     * @var array
+     */
+    protected $_importErrors = array();
 
-	/**
-	 * Define main table and id field name
-	 *
-	 * @return void
-	 */
-	protected function _construct()
-	{
-		$this->_init('mmshippingplus/shippingplus', 'mmshippingplus_id');
-	}
+    /**
+     * Define main table and id field name
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('mmshippingplus/shippingplus', 'mmshippingplus_id');
+    }
 
-	/**
-	 * Upload CSV file of Tablerate
-	 *
-	 * @see MMind_Shippingplus_Model_Config_Shippingplus._afterSave()
-	 * @param Varien_Object $object
-	 * @throws Mage_Core_Exception
-	 * @return MMind_Shippingplus_Model_Resource_Shippingplus_Import
-	 ***/
-	public function uploadAndImport(Varien_Object $object)
-	{
-		if (empty($_FILES['groups']['tmp_name']['mmshippingplus']['fields']['import_tablerate']['value']))
-			return $this;
+    /**
+     * Upload CSV file of Tablerate
+     *
+     * @see MMind_Shippingplus_Model_Config_Shippingplus._afterSave()
+     * @param Varien_Object $object
+     * @throws Mage_Core_Exception
+     * @return MMind_Shippingplus_Model_Resource_Shippingplus_Import
+     ***/
+    public function uploadAndImport(Varien_Object $object)
+    {
+        if (empty($_FILES['groups']['tmp_name']['mmshippingplus']['fields']['import_tablerate']['value']))
+            return $this;
 
-		$csvFile = $_FILES['groups']['tmp_name']['mmshippingplus']['fields']['import_tablerate']['value'];
-		$website = Mage::app()->getWebsite($object->getScopeId());
+        $csvFile = $_FILES['groups']['tmp_name']['mmshippingplus']['fields']['import_tablerate']['value'];
+        $website = Mage::app()->getWebsite($object->getScopeId());
 
-		$this->_importWebsiteId = (int)$website->getId();
+        $this->_importWebsiteId = (int)$website->getId();
 
-		$io = new Varien_Io_File();
-		$info = pathinfo($csvFile);
-		$io->open(array('path' => $info['dirname']));
-		$io->streamOpen($info['basename'], 'r');
+        $io = new Varien_Io_File();
+        $info = pathinfo($csvFile);
+        $io->open(array('path' => $info['dirname']));
+        $io->streamOpen($info['basename'], 'r');
 
-		// check and skip headers
-		$headers = $io->streamReadCsv();
-		if ($headers === false || count($headers) < $this->tablerate_cols) {
-			$io->streamClose();
-			Mage::throwException(Mage::helper('mmshippingplus')->__('Invalid CSV Table Rates File Format'));
-		}
+        // check and skip headers
+        $headers = $io->streamReadCsv();
+        if ($headers === false || count($headers) < $this->tablerate_cols) {
+            $io->streamClose();
+            Mage::throwException(Mage::helper('mmshippingplus')->__('Invalid CSV Table Rates File Format'));
+        }
 
-		$adapter = $this->_getWriteAdapter();
-		$adapter->beginTransaction();
+        $adapter = $this->_getWriteAdapter();
+        $adapter->beginTransaction();
 
-		try {
-			$rowNumber = 1;
-			$importData = array();
+        try {
+            $rowNumber = 1;
+            $importData = array();
 
-			// delete old data by website and condition name
-			$condition = array('website_id = ?' => $this->_importWebsiteId,);
-			$adapter->delete($this->getMainTable(), $condition);
+            // delete old data by website and condition name
+            $condition = array('website_id = ?' => $this->_importWebsiteId,);
+            $adapter->delete($this->getMainTable(), $condition);
 
-			while (false !== ($csvLine = $io->streamReadCsv())) {
-				$rowNumber++;
+            while (false !== ($csvLine = $io->streamReadCsv())) {
+                $rowNumber++;
+                $nomulti = false;
 
-				if (empty($csvLine)) {
-					continue;
-				}
+                if (empty($csvLine)) {
+                    continue;
+                }
 
-				$row = $this->_getImportRow($csvLine, $rowNumber);
+                $row = $this->_getImportRow($csvLine, $rowNumber);
 
-				if ($row !== false) {
-					// Import with Multi country
-					$countries = explode("|", $row[1]);
-					foreach ($countries as $country) {
-						$row[1] = $country;
-						$importData[] = $row;
-					}
-				}
+                if ($row !== false) {
+                    // Import with Multi country
+                    $countries = explode("|", $row[1]);
+                    foreach ($countries as $country) {
+                        $row[1] = $country;
 
-				if (count($importData) == 5000) {
-					$this->_saveImportData($importData);
-					$importData = array();
-				}
+                        // Import with Multi Region Code
+                        $regions = explode("|", $row[2]);
+                        foreach ($regions as $region) {
+                            $row[2] = $region;
 
-			}
-			$this->_saveImportData($importData);
-			$io->streamClose();
-		} catch (Mage_Core_Exception $e) {
-			$io->streamClose();
-			Mage::throwException($e->getMessage());
-		} catch (Exception $e) {
-			$io->streamClose();
-			Mage::logException($e);
-			Mage::throwException(Mage::helper('mmshippingplus')->__('An error occurred while import CSV Table rates.'));
-		}
+                            // Import with Multi ZIP Code
+                            $zips = explode("|", $row[3]);
+                            foreach ($zips as $zip) {
+                                $row[3] = $zip;
+                                $importData[] = $row;
+                            }
+                        }
+                    }
+                }
 
-		$adapter->commit();
+                if (count($importData) == 5000) {
+                    $this->_saveImportData($importData);
+                    $importData = array();
+                }
 
-		if ($this->_importErrors) {
-			$error = Mage::helper('mmshippingplus')->__('%1$d records have been imported. See the following list of errors for each record that has not been imported: %2$s', $this->_importedRows, implode(" \n", $this->_importErrors));
-			Mage::throwException($error);
-		}
+            }
+            $this->_saveImportData($importData);
+            $io->streamClose();
+        } catch (Mage_Core_Exception $e) {
+            $io->streamClose();
+            Mage::throwException($e->getMessage());
+        } catch (Exception $e) {
+            $io->streamClose();
+            Mage::logException($e);
+            Mage::throwException(Mage::helper('mmshippingplus')->__('An error occurred while import CSV Table rates.'));
+        }
 
-		return $this;
+        $adapter->commit();
 
-	}
+        if ($this->_importErrors) {
+            $error = Mage::helper('mmshippingplus')->__('%1$d records have been imported. See the following list of errors for each record that has not been imported: %2$s', $this->_importedRows, implode(" \n", $this->_importErrors));
+            Mage::throwException($error);
+        }
 
-	/**
-	 * Validate row for import and return table rate array or false
-	 * Error will be add to _importErrors array
-	 *
-	 * @param array $row
-	 * @param int $rowNumber
-	 * @return array|false
-	 */
-	protected function _getImportRow($row, $rowNumber = 0)
-	{
-		// validate row
-		if (count($row) < $this->tablerate_cols) {
-			$this->_importErrors[] = Mage::helper('mmshippingplus')->__('Invalid CSV Table Rates format in the Row #%s', $rowNumber);
-			return false;
-		}
+        return $this;
 
-		// strip whitespace from the beginning and end of each row
-		foreach ($row as $k => $v) {
-			$row[$k] = trim($v);
-		}
+    }
 
-		return array(
-			$this->_importWebsiteId,
-			$row[0],
-			$row[1],
-			$row[2],
-			$row[3],
-			str_replace(',', '.', $row[4]),
-			$row[5],
-			$row[6]
-		);
-	}
+    /**
+     * Validate row for import and return table rate array or false
+     * Error will be add to _importErrors array
+     *
+     * @param array $row
+     * @param int $rowNumber
+     * @return array|false
+     */
+    protected function _getImportRow($row, $rowNumber = 0)
+    {
+        // validate row
+        if (count($row) < $this->tablerate_cols) {
+            $this->_importErrors[] = Mage::helper('mmshippingplus')->__('Invalid CSV Table Rates format in the Row #%s', $rowNumber);
+            return false;
+        }
 
-	/**
-	 * Save import data batch
-	 *
-	 * @param array $data
-	 * @return MMind_Shippingplus_Model_Resource_Shippingplus_Import
-	 */
-	protected function _saveImportData(array $data)
-	{
-		if (!empty($data)) {
-			$columns = array(
-				'website_id',
-				'dest_country',
-				'dest_region',
-				'dest_zip',
-				'weight',
-				'price',
-				'charge',
-				'type'
-			);
-			$this->_getWriteAdapter()->insertArray($this->getMainTable(), $columns, $data);
-			$this->_importedRows += count($data);
-		}
-		return $this;
-	}
+        // strip whitespace from the beginning and end of each row
+        foreach ($row as $k => $v) {
+            $row[$k] = trim($v);
+        }
+
+        return array(
+            $this->_importWebsiteId,
+            $row[0],
+            $row[1],
+            $row[2],
+            $row[3],
+            str_replace(',', '.', $row[4]),
+            $row[5],
+            $row[6]
+        );
+    }
+
+    /**
+     * Save import data batch
+     *
+     * @param array $data
+     * @return MMind_Shippingplus_Model_Resource_Shippingplus_Import
+     */
+    protected function _saveImportData(array $data)
+    {
+        if (!empty($data)) {
+            $columns = array(
+                'website_id',
+                'dest_country',
+                'dest_region',
+                'dest_zip',
+                'weight',
+                'price',
+                'charge',
+                'type'
+            );
+            $this->_getWriteAdapter()->insertArray($this->getMainTable(), $columns, $data);
+            $this->_importedRows += count($data);
+        }
+        return $this;
+    }
 }
